b0bd50648f2c05bc77dd24491d030b66fc9b4c9a
raboin09 <raboin09@gmail.com>
2017-09-28 11:12:12 -0500

    Initial Commit

---
+++ NaiveBayes.R
@@ -0,0 +1,57 @@
+##A skeleton for implementing Naive Bayes Classifier in R
+## Author: Salem 
+
+library(matrixStats)
+
+##You can use setwd("C:/YourPATH here/") to set the working directory..
+setwd("C:/Users/james.raboin/Desktop/NB/")
+
+trainingFile = "irisTraining.txt"
+testingFile = "irisTesting.txt"
+Xtrain = as.matrix(read.table(trainingFile))
+n = dim(Xtrain)[1]
+d = dim(Xtrain)[2] - 1 # the last attribute is the class label, so it does not count.
+##Training... Collect mean and standard deviation for each dimension for each class..
+##Also, calculate P(C+) and P(C-)
+idp = which(Xtrain[,d+1] ==1) # points that have 1 as the class label
+np = length(idp)
+Xpositive = Xtrain[idp,1:d]
+
+##Testing .....
+Xtest=as.matrix(read.table(testingFile))
+nn = dim(Xtest)[1] # Number of points in the testing data.
+
+tp = 0 #True Positive
+fp = 0 #False Positive
+tn = 0 #True Negative
+fn = 0 #False Negative
+
+print(colnames(Xtrain))
+
+cm <- colMeans(Xtrain)
+
+print(cm)
+
+csd <- colSds(Xtrain)
+
+print(csd)
+
+posClass = 0
+
+negClass = 0
+
+totalClass = 0
+
+for(currentInt in Xtrain[, "V5"]){
+  if(currentInt > 0){ posClass = posClass+1 }
+  else { negClass = negClass+1 }
+  totalClass = totalClass + 1
+}
+
+print(idp)
+
+print(posClass/100) #P(C+)
+
+print(negClass/100) #P(C-)
+
+print(totalClass)
