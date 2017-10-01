f5d03c7555102c2fee1268a798cd50c2154f202f
raboin09 <raboin09@gmail.com>
2017-10-01 15:39:27 -0500

    Added functions, currently testing iterations in R

--- NaiveBayes.R
+++ NaiveBayes.R
@@ -1,10 +1,12 @@
 ##A skeleton for implementing Naive Bayes Classifier in R
 ## Author: Salem 
 
+##install.packages("matrixStats")
+
 library(matrixStats)
 
 ##You can use setwd("C:/YourPATH here/") to set the working directory..
-setwd("C:/Users/james.raboin/Desktop/NB/")
+setwd("C:/Users/james.raboin/Desktop/NaiveBayesR-Master/")
 
 trainingFile = "irisTraining.txt"
 testingFile = "irisTesting.txt"
@@ -26,46 +28,51 @@ fp = 0 #False Positive
 tn = 0 #True Negative
 fn = 0 #False Negative
 
-print(colnames(Xtrain))
-
 cm <- colMeans(Xtrain)
-
-print(cm)
 
 csd <- colSds(Xtrain)
 
-print(csd)
+posClass = Xtrain[idp, 1:d]
+posMeans = colMeans(posClass)
+posStdDev = colSds(posClass)
+posTotalProb = length(posClass[,"V1"])/length(Xtrain[,"V1"])
 
-posClass = 0
+negClass = Xtrain[-idp, 1:d]
+negMeans = colMeans(negClass)
+negStdDev = colSds(negClass)
+negTotalProb = length(negClass[,"V1"])/length(Xtrain[,"V1"])
 
-negClass = 0
+#print(posMeans[1])
+#print(posStdDev[1])
 
-totalClass = 0
+#print(negMeans[1])
+#print(negStdDev[1])
 
-for(currentInt in Xtrain[, "V5"]){
-  if(currentInt > 0){ posClass = posClass+1 }
-  else { negClass = negClass+1 }
-  totalClass = totalClass + 1
-}
+#print(negTotalProb)
+#print(posTotalProb)
 
-print(idp)
+#print(posClass[1, "V1"])
 
-print(posClass/100) #P(C+)
+#testFunc <- function(a, b) a + b
+#apply(dat[,c('x','z')], 1, function(x) testFunc(x[1],x[2]))
 
-print(negClass/100) #P(C-)
+singleColFunc <- function(colVal, colStd, colMean) dnorm(colVal, colMean, colStd)
+totalColFunc <- function(col1, col2, col3, col4) prod(col1, col2, col3, col4)
 
-print(totalClass)
-
-print(csd[1])
-
-inv <- ((1/(sqrt(2*pi)*csd[1])))*exp(((4.6 - cm[1])^2)/(2*(csd[1]^2)))
-
-print(inv)
-
-print(Xtrain[58, "V1"])
-
-prob <- dnorm(Xtrain[,"V1"], cm[1], csd[1])
+prob <- singleColFunc(posClass[1, "V1"], posStdDev[1], posMeans[1])
 
 print(prob)
+
+#posprob <- dnorm(posClass[1, "V1"], posMeans["V1"], posStdDev[1]) * dnorm(posClass[1, "V2"], posMeans["V2"], posStdDev[2]) * dnorm(posClass[1, "V3"], posMeans["V3"], posStdDev[3]) * dnorm(posClass[1, "V4"], posMeans["V4"], posStdDev[4])
+
+#negprob <- dnorm(posClass[1, "V1"], negMeans["V1"], negStdDev[1]) * dnorm(posClass[1, "V2"], negMeans["V2"], negStdDev[1]) * dnorm(posClass[1, "V3"], negMeans["V3"], negStdDev[1]) * dnorm(posClass[1, "V4"], negMeans["V4"], negStdDev[1])
+
+#applyProbs <- apply(posClass[,"V1"], 2, function(x) singleColFunc(posClass, posStdDev, posMeans))
+
+#print(applyProbs)
+
+#print(posprob)
+
+#prob <- dnorm(Xtrain[,"V1"], cm[1], csd[1])
 
 
